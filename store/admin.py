0f2b41cba0d2b17a2194e3cc65b28383533b0dff
italocarvalhoick <italocarvalhoick@gmail.com>
2023-02-02 15:16:36 -0300

    more

--- store/admin.py
+++ store/admin.py
@@ -17,7 +17,7 @@ class ProductAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Customer)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Cart)
 admin.site.register(Cartitems)
 admin.site.register(ShippingAddress)
