59828fa9113140fa52c242b06be2b1b6e01f7257
italocarvalhoick <italocarvalhoick@gmail.com>
2023-02-02 15:12:41 -0300

    more

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from . models import *
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'image')
+    
+    list_display_links = ('id','name', 'price')
+
+    #list_filter = ('nome', 'sobrenome')
+
+    list_per_page = 10
+
+    search_fields = ('name', 'price')
+
+
+
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
