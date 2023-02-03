ed7b3d767f62727750fd6451fd7208787f21e401
italocarvalhoick <italocarvalhoick@gmail.com>
2023-02-03 11:20:08 -0300

    more

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'store.context_processors.produto_search',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -64,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.context_processors.product_links',
+                'store.context_processors.perfume_search',
             ],
         },
     },
